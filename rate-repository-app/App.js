79317c9e9a37a425ba74525aafef6359ff2285e7
xhello00o <133972573+xhello00o@users.noreply.github.com>
2023-09-10 14:27:12 +0800

    retrieving repository fetch from httpServer

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,12 +1,20 @@
 import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 
 const App = () => {
   return (
+    
     <NativeRouter>
+      <ApolloProvider client={apolloClient}>
       <Main />
-    </NativeRouter>    
+      </ApolloProvider> 
+    </NativeRouter>   
+    
   );
 };
 
